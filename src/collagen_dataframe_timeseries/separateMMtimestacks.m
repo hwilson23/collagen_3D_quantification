bb4215a3ec98e21618f35aff4ca3ca1ee8f6f2e3
hwilson23 <111527077+hwilson23@users.noreply.github.com>
2026-06-10 13:16:21 -0500

    fix for looping through files

--- src/collagen_dataframe_timeseries/separateMMtimestacks.m
+++ src/collagen_dataframe_timeseries/separateMMtimestacks.m
@@ -1,34 +1,48 @@
-%separate timepoints from stack
+folder = 'G:\FluorescentCollagen\20260519_flucol_kpc_ows3\20260519_hoescht_col_channels\';
+outpath = "G:\FluorescentCollagen\20260519_flucol_kpc_ows3\20260519_hoescht_col_channels\maskapplied_timeseries_stacks\";
+numz = 46;
+files = dir(fullfile(folder, 'sub7000*.tif'));
 
-file = "G:\FluorescentCollagen\20250518_otherpos\sub7500_flucol594_kpcclusters_800nm_poc0_60745_20x_g55_1_MMStack_Pos4.ome-1.tif";
-name = "sub7500_flucol594_kpcclusters_800nm_poc0_60745_20x_g55_1_MMStack_Pos4.ome-1.tif";
-outpath = "G:\FluorescentCollagen\20250518_otherpos\maskapplied_timeseries_stacks\";
+for k = 1:numel(files)
 
-imgpg1 = imread(file);
-[numrow, numcols, ~]  = size(imgpg1);
-info = imfinfo(file);
-numpages = length(info);
-numpages = numpages/25;
+    % Full path to input file
+    file = fullfile(files(k).folder, files(k).name);
 
-for time = 1:25
+    % Original filename
+    name = files(k).name;
+
+    % Filename without extension
+    [~, baseName, ~] = fileparts(name);
+
+    fprintf('Processing %s\n', name);
+
+
+%file = "G:\FluorescentCollagen\20260519_flucol_kpc_ows3\20260519_hoescht_col_channels\sub7000C1-flucol594_bkokpcwhoescht_800nm_blank_44530_37010_blank_g558555_poc0_1_MMStack_Pos2.tif"
+%name = "sub7000C1-flucol594_bkokpcwhoescht_800nm_blank_44530_37010_blank_g558555_poc0_1_MMStack_Pos1.tif";
+
+    imgpg1 = imread(file);
+    [numrow, numcols, ~]  = size(imgpg1);
+    info = imfinfo(file);
+    numpages = length(info);
+    numpages = numpages/25;
     
-    filenameout = join([outpath name '_t' num2str(time) '.tif']);
-    startpage = (time-1)*28 +1;
-    for z = 1:28
-        page = startpage +z -1;
-
-        volimg = imread(file,page);
-        volimg = double(volimg);
-        disp(size(volimg))
+    for time = 1:25
         
-        if z ==1
-            imwrite(uint16(volimg),filenameout);
-        else
-            imwrite(uint16(volimg), filenameout,'WriteMode','append')
+        filenameout = join([outpath name '_t' num2str(time) '.tif']);
+        startpage = (time-1)*numz +1;
+        for z = 1:numz
+            page = startpage +z -1;
+    
+            volimg = imread(file,page);
+            volimg = double(volimg);
+            disp(size(volimg))
+            
+            if z ==1
+                imwrite(uint16(volimg),filenameout);
+            else
+                imwrite(uint16(volimg), filenameout,'WriteMode','append')
+            end
         end
+        
     end
-    
- end
-    
-
-    
+end
